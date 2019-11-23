91ac7b858d82e8877a14a39d734f81eedb2099df
gaoyishu <gaoyishu91@163.com>
2019-11-23 10:36:52 +0800

    re-deployed

---
+++ src/main/java/life/hk/community/controller/HelloController.java
@@ -0,0 +1,21 @@
+package life.hk.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * User: gaoyishu
+ * Date: 2019/11/23
+ * Time: 10:10
+ */
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+
+    }
+}
