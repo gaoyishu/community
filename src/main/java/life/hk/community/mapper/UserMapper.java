014dba73dd8a76d77134e2299a3a8653d2af0a07
gaoyishu <gaoyishu91@163.com>
2019-12-20 10:49:04 +0800

    'h2_database'

--- src/main/java/life/hk/community/mapper/UserMapper.java
+++ src/main/java/life/hk/community/mapper/UserMapper.java
@@ -15,3 +15,4 @@ public interface UserMapper {
     @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{Modified})")
     void insert(User user);
 }
+
