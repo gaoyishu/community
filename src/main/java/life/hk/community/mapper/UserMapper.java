d33c7b46a2ce27dda7deec4cdb785737ef7b9c91
gaoyishu <gaoyishu91@163.com>
2019-12-20 11:30:13 +0800

    h2_database_insert

--- src/main/java/life/hk/community/mapper/UserMapper.java
+++ src/main/java/life/hk/community/mapper/UserMapper.java
@@ -12,7 +12,7 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserMapper {
-    @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{Modified})")
+    @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
 }
 
